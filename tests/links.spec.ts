67642e1d0a23defba4c2ec6727b4a6ba8e90ae9f
ArtsiukhoyskayaMargo <margaryta.artsiukhouskaya@innowise.com>
2024-07-05 16:06:38 +0300

    Added a links tests

--- tests/links.spec.ts
+++ tests/links.spec.ts
@@ -1,39 +1,29 @@
 import { expect, test } from '@playwright/test'
 
-test('test switching to a tab Elements', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('/');
-	await page.locator('//h5[text()="Elements"]').click()
+	await page.getByText('Elements').click()
 	await page.locator('//span[text()="Links"]').click()
-})
+});
 
-// test('test clicking Created link and checking 201 status code', async ({ page }) => {
-//    await page.goto('https://demoqa.com/links')////a[@id='created'][text()="Created"]
 
-// 	await page.route('**/*', (route, request) => {
-// 		route.continue((response) => {
-// 		  if (response.status() === 201) {
-// 			 console.log('Request succeeded with status 201');
-// 		  }
-// 		  return response;
-// 		});
-// 	 });
-  
-// 	 // Клик по ссылке
-// 	 await page.locator('#created').click();
-  
-// 	 // Проверка что статус код 201 был получен
-// 	 const response = await page.waitForResponse(response => response.status() === 201);
-// 	 expect(response.status()).toBe(201);
-//   });
-// })
+test('test checking Home link', async ({ page, context }) => {
+		//Мы используем Promise.all для параллельн.д. открытие новой вкладки+клик по ссылке
+		const [newPage] = await Promise.all([
+		  context.waitForEvent('page'),
+		  page.locator('#simpleLink').click(), 
+		]);
+		await expect(newPage).toHaveURL('https://demoqa.com/');
+	 });
 
-// // test.describe('NASA APOD API Filtering and Searching', () => {
-// // 	const apiKey = 'za6Lneqxy2TErym1ZODR8ZDML0qcvD1tqBUxMeXC'; // Replace with a valid API key for real testing
-// // 	const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=2022-01-01&end_date=2022-12-31`;
- 
-// // 	test('TC-001: ', async ({ request }) => {
-// // 	  // Step 1: Send a GET request to the API endpoint with date range parameters
-// // 	  const response = await request.get(apiUrl);
- 
-// // 	  // Step 2: Verify the status code of the response
-// // 	  expect(response.status()).toBe(200);
+ test('Сlick on the link Created and check for 201 status code', async ({ page }) => {
+	// Шаг 1: Переход на нужную страницу
+    await page.goto('/links');
+    // Шаг 2: Клик по ссылке с идентификатором #created
+    await page.locator('#created').click();
+    // Шаг 3: Ожидание получения ответа с сервера с статус кодом 201
+    const response = await page.waitForResponse(response => response.status() === 201);
+    // Шаг 4: Проверка, что статус код действительно 201
+    expect(response.status()).toBe(201);
+    expect(await page.locator('#linkResponse')).toContainText('201')
+  });
--- tests/radioButton.spec.ts
+++ tests/radioButton.spec.ts
@@ -1,11 +1,21 @@
 import { expect, test } from '@playwright/test'
 
-test('test Radio Button', async ({ page }) => {
-	await page.goto('/');
-	await page.getByText('Elements').click();
-	await page.locator('//span[text()="Radio Button"]').click()
-	await page.locator('label[class^="custom-control"][for="yesRadio"]').click()
-	await expect(page.locator('//p[@class="mt-3"]').textContent()).resolves.toBe("You have selected Yes");
-	await page.locator('label[class^="custom-control"][for="impressiveRadio"]').click()
-	await expect(page.locator('//p[@class="mt-3"]').textContent()).resolves.toBe("You have selected Impressive");
-})
+test.describe("Radio buttons", () => {
+	const yesRadioResult = 'Yes';
+
+	test.beforeEach(async ({ page }) => {
+		 await page.goto('/radio-button');
+	});
+
+	test("Click on Yes radio", async ({ page }) => {
+		 await page.locator('label[for="yesRadio"]').check(); //Использует метод check() для выбора (установки) этой радиокнопки.
+		 await page.locator('#yesRadio').isChecked(); //проверяет, что радиокнопка с идентификатором #yesRadio действительно находится в состоянии "выбрано".
+		 expect(await page.locator('.text-success')).toHaveText(yesRadioResult); //Убеждается, что соответствующий текст на странице обновился до 'Yes'.
+	});
+});
+
+// test("Click on Yes radio", async ({ page }) => {
+// 	await page.locator('#yesRadio').check({ force: true });
+// 	await page.locator('#yesRadio').isChecked();
+// 	expect(await page.locator('.text-success')).toHaveText(yesRadioResult);
+// });
