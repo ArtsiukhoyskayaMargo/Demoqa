b1e06534048656c4d07447f6838b54d98e6c8593
ArtsiukhoyskayaMargo <margaryta.artsiukhouskaya@innowise.com>
2024-07-09 10:34:13 +0300

    added test uploading a file

--- tests/uploadAndDownload.spec.ts
+++ tests/uploadAndDownload.spec.ts
@@ -2,11 +2,14 @@ import { expect, test } from '@playwright/test'
 import fs from 'fs'; //Модуль Node.js для работы с файловой системой.
 import path from 'path'; //Модуль Node.js для работы с путями файловой системы.
 
-test('test downloading a file', async ({ page }) => {
-		await page.goto('/');
-		await page.getByText('Elements').click()
-		await page.getByText('Upload and Download').click()
+test.beforeEach(async ({ page }) => {
+	await page.goto('/');
+	await page.getByText('Elements').click()
+	await page.getByText('Upload and Download').click()
 
+});
+
+test('test downloading a file', async ({ page }) => {
 		const downloadPromise = page.waitForEvent('download');
 		await page.locator('#downloadButton').click();
 		const download = await downloadPromise;
@@ -24,3 +27,16 @@ test('test downloading a file', async ({ page }) => {
 		  throw new Error(`Файл ${filePath} не был найден.`);
 		}
 	 });
+
+	 test('test uploading a file', async ({ page }) => {
+	  //await page.locator('#uploadFile').click();
+	  const filePath = path.resolve('C:', 'Users', 'User', 'OneDrive', 'Рабочий стол', 'gpt.txt');
+
+	  // Найдите элемент <input type="file"> и установите файл
+	  await page.setInputFiles('#uploadFile', filePath);
+
+	 // Проверка, что файл был загружен
+	 const fileName = await page.$eval('#uploadFile', input => input.files[0].name);
+	 expect(fileName).toBe('gpt.txt'); // Убедитесь, что имя файла совпадает
+	 
+});
