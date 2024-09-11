108dac5bfe0b34174df7c8527d56b402355971cd
ArtsiukhoyskayaMargo <margaryta.artsiukhouskaya@innowise.com>
2024-09-11 11:28:49 +0300

    Commit

--- tests/accordian.spec.ts
+++ tests/accordian.spec.ts
@@ -6,6 +6,6 @@ test('check that text appears when clicking "What is Lorem Ipsum?"', async ({ pa
 	await page.getByText('Accordian').click();
 	await page.getByText('What is Lorem Ipsum?').click();
 	const locator = page.locator('#section1Content');
-	await expect(locator).toContainText("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+	await expect(locator).toContainText("Lorem Ipsum is simply dummy text of the printing and typesetting industry!.");
 });
 
